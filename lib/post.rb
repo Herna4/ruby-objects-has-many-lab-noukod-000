95fa72869f7573174e04cf9b6ad92c733e7052c7
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-23 15:06:55 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,36 @@
+# class Post
+#
+#   attr_accessor :title, :author
+#
+#   def initialize(title)
+#     @title = title
+#   end
+#
+#
+#
+#   def author_name
+#     if @author == nil
+#       return nil
+#     else
+#       @author.name
+#     end
+#   end
+# end
+
+
+class Post
+  attr_accessor :author, :title
+
+   def initialize(title)
+    @title = title
+  end
+
+   def author_name
+    if author
+      self.author.name
+    else
+      nil
+    end
+  end
+
+ end
